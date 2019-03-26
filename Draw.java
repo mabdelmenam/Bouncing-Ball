d4692d8b759b535989a9c2373509910487fd1bf7
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2019-03-25 20:47:03 -0400

    Implemented Runnable object in Draw class Attempting to bounce ball off of walls
    Added a MouseListener, along with mouseClicked method in order to signal
    when to start moving the ball.
    Created new Thread

--- Draw.java
+++ Draw.java
@@ -1,15 +1,64 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-public class Draw extends JPanel{
+public class Draw extends JPanel implements Runnable{
 
-    public void drawing(){
-        repaint();//repaint calls the paintComponent method
+    private int x = 10;
+    private int y = 10;
+    private int universalSpeed = 3;
+    private int xspeed = 3;
+    private int yspeed = 3;
+    private int ball = 30;
+
+    public boolean bottom(){
+        return ( ( this.y + this.ball ) >= this.getHeight() ); 
     }
+    public boolean left()
+	{
+		return ( this.x <= 0 );
+	}
+
+	public boolean right()
+	{
+		return ( ( this.x + this.ball ) >= this.getWidth() );
+	}
+
+	public boolean top()
+	{
+		return ( this.y <= 0 );
+    }
+    
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        g.fillOval(0, 0, 30, 30);
+        g.fillOval(x, y, this.ball, this.ball);
+    }
+    public void run(){
+        while(true){
+            //repaint();
+            try{
+                Thread.sleep(100);
+            }catch(Exception e){}
+
+            if(this.left()){
+                this.xspeed = this.universalSpeed;
+            }
+            else if(this.right()){
+                this.xspeed -= this.universalSpeed;
+            }
+            
+            if(this.top()){
+                this.yspeed = this.universalSpeed;
+            }
+            else if(this.bottom()){
+                this.yspeed -= this.universalSpeed;
+            }
+            System.out.println(this.x + " " + this.y);
+
+            this.y += this.yspeed;
+           repaint();
+
+        }
     }
 }
--- J11.java
+++ J11.java
@@ -1,7 +1,34 @@
 import javax.swing.JFrame;
 
-public class J11 {
+import java.awt.Graphics;
+
+import javax.swing.JButton;
+import java.applet.Applet;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class J11{
+
+    private static int width = 300;
+    private static int height = 300;
     public static void main(String args[]){
-        System.out.println("Hello");
+
+        JFrame frame = new JFrame("J.11 - Bouncing Ball");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(J11.width, J11.height);
+        frame.setVisible(true);
+
+        Draw d = new Draw();
+        frame.add(d);
+
+        frame.addMouseListener( new MouseAdapter()
+        {
+            @Override
+            public void mouseClicked(MouseEvent e){
+                System.out.println("Clicked");
+                Thread t = new Thread(d);
+                t.run();
+            }
+        });
     }
 }
